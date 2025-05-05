4f5593aea9528606df340c0540479eb156effa7a
Alex Lescionok <alex.lescionok@tes.com>
2025-05-05 15:29:10 +0100

    added Terraform code for AWS infra

---
+++ terraform/00_terraform.tf
@@ -0,0 +1,43 @@
+locals {
+  default_region = "REPLACE_ME"
+  default_tags = {
+    /*
+    Default tags you wish to apply across all resources provisioned by terraform, for example:
+    environment = "production"
+    project     = "core-components"
+    */
+  }
+}
+
+terraform {
+
+  required_version = "~> 1.11.0"
+
+  backend "s3" {
+    bucket = "REPLACE_ME"
+    region = "REPLACE_ME"
+    key    = "REPLACE_ME"
+
+    encrypt      = true
+    use_lockfile = true
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.97.0"
+    }
+  }
+}
+
+### Default provider
+provider "aws" {
+  region                   = local.default_region
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  profile                  = "REPLACE_ME"
+  default_tags {
+    tags = local.default_tags
+  }
+}
